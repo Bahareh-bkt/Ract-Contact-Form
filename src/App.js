d39dae13c108649300707b1a8d7ce4286630b454
Bahareh Bakhtiari <bahareh4@gmail.com>
2022-05-27 14:07:25 +0430

    last changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import NotesList from "./components/NotesList";
+import { useState } from "react";
+import {nanoid} from 'nanoid';
+/*import AddNote from "./components/AddNote";*/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>{
+    const [notes, setNotes] = useState([
+        {
+            id: nanoid(),
+            text: "Das ist mein erstes Note!",
+            date: "27.05.2022"
+        },
+        {
+            id: nanoid(),
+            text: "Hallo ",
+            date: "21.05.2022"
+        },
+        {
+            id: nanoid(),
+            text: " Welt",
+            date: "22.05.2022"
+        },
+        {
+            id: nanoid(),
+            text: "Hallo-Welt",
+            date: "23.05.2022"
+        },
+        {
+            id: nanoid(),
+            text: "Hallo,Welt",
+            date: "24.05.2022"
+        },
+        {
+            id: nanoid(),
+            text: "Hallo Welt",
+            date: "26.05.2022"
+        },
+    ]);
+
+    const addNote = (text) =>{
+        const date = new Date()
+        const newNote = {
+            id: nanoid(),
+            text: text,
+            date: date.toLocaleDateString()
+        }
+    
+    const newNotes= [...notes, newNote]
+    setNotes(newNotes)
+    }
+    const deleteNote= (id) =>{
+       const newNotes = notes.filter((note)=> note.id !== id)
+       setNotes(newNotes)
+    }
+
+
+return (
+    <div className="container">
+        <NotesList notes={notes}
+        handleAddNote={addNote}
+        handleDeleteNote = {deleteNote}
+        />
     </div>
-  );
+    )
 }
 
 export default App;
